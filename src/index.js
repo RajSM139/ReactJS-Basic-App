86725f13d8ac36b95163905a0aeaaab40353e866
Rajesh Mishra <rajesh@studiographene.com>
2021-06-23 13:19:04 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+const API_KEY = 'AIzaSyD9r8xNj-FRs6jJ2KTdqk_f_TOxOe17UIM';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('king');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
